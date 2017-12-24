03396ce2f9d26b9b1b8b67ca0e2697497c5475da
jforbes <jeannette.forbes@gmail.com>
2017-12-24 15:00:24 -0800

    character schema added

---
+++ server/schema/character.js
@@ -0,0 +1,54 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let characterSchema = new Schema({
+    meta: {
+        created: Date
+    },
+    name: String,
+    player: String,
+    pointsTotal: Number,
+    pointsUnspent: Number,
+    description: {
+        height      : String,
+        weight      : String,
+        sizeModifier: Number,
+        age         : Number,
+        appearance  : String
+    },
+    stats: {
+        ST  : Number,
+        DX  : Number,
+        IQ  : Number,
+        HT  : Number,
+        HP  : Number,
+        Will: Number,
+        Per : Number,
+        FP  : Number
+    },
+    advantages   : [String],
+    disadvantages: [String],
+    skills       : [String],
+    inventory: {
+        meleeWeapons : [String],
+        rangedWeapons: [String],
+        armor        : [String],
+        possessions  : [String]
+    }
+});
+
+characterSchema.methods.findByPlayer = (cb) => {
+    return this.model('Character').find({ player: this.player }, cb);
+};
+
+characterSchema.methods.findByPointsTotal = (cb) => {
+    return this.model('Character').find({ pointsTotal: this.pointsTotal }, cb);
+}
+
+characterSchema.methods.findByIdAndUpdate = (cb) => {
+    // Does this character
+}
+
+let Character = mongoose.model('Character', characterSchema);
+
+module.exports = Character;
